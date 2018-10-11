25e09dc4d19d4029552d55e0ad820efba21b6028
Hamill <1544152390@mil>
2018-10-11 15:29:14 -0400

    clean up, added option for label specific thresholds

--- create_library/retile.py
+++ create_library/retile.py
@@ -8,48 +8,61 @@
 from __future__ import division
 from joblib import Parallel, delayed
 from glob import glob
-import numpy as np 
+import numpy as np
 from imageio import imread
 from scipy.io import loadmat
 import sys, getopt, os
-
 from tile_utils import *
-
+import csv
 from scipy.stats import mode as md
 from scipy.misc import imsave
 
 if sys.version[0]=='3':
-   from tkinter import Tk, Toplevel 
+   from tkinter import Tk, Toplevel
    from tkinter.filedialog import askopenfilename
    import tkinter
    import tkinter as tk
-   from tkinter.messagebox import *   
+   from tkinter.messagebox import *
    from tkinter.filedialog import *
 else:
    from Tkinter import Tk, TopLevel
    from tkFileDialog import askopenfilename
    import Tkinter as tkinter
    import Tkinter as tk
-   from Tkinter.messagebox import *   
-   from Tkinter.filedialog import *   
-   
+   from Tkinter.messagebox import *
+   from Tkinter.filedialog import *
+
 import os.path as path
 
 
 # =========================================================
 def writeout(tmp, cl, labels, outpath, thres):
-
+   '''
+   tmp = tile from jpg image
+   cl = tile from label image_names
+   labels = list of label strings
+   outpath = directory to write organize tiles in
+   '''
+   np.unique(cl)
+   #look for most common classification in tile
    l, cnt = md(cl.flatten())
    l = np.squeeze(l)
-   if cnt/len(cl.flatten()) > thres:
-      outfile = id_generator()+'.jpg'
-      fp = outpath+os.sep+labels[l]+os.sep+outfile
-      imsave(fp, tmp)
+
+   if type(thres) == dict:
+      if cnt/len(cl.flatten()) > thres[int(l)]:
+         outfile = id_generator()+'.jpg'
+         fp = os.path.normpath(outpath+os.sep+labels[l]+os.sep+outfile)
+         imsave(fp, tmp)
+   else:
+      if cnt/len(cl.flatten()) > thres:
+         outfile = id_generator()+'.jpg'
+         fp = os.path.normpath(outpath+os.sep+labels[l]+os.sep+outfile)
+         imsave(fp, tmp)
 
 #==============================================================
 if __name__ == '__main__':
 
-   direc = ''; tile = ''; thres = ''
+   direc = ''; tile = ''; thres = ''; thin=''
 
    argv = sys.argv[1:]
    try:
@@ -68,29 +81,42 @@ if __name__ == '__main__':
          thres = arg
       elif opt in ("-b"):
          thin = arg
-		 
+
    if not direc:
-      direc = 'train'
+      direc = r'data\\train'
    if not tile:
       tile = 96
    if not thres:
       thres = .9
    if not thin:
       thin = 0
-	  
+
    tile = int(tile)
-   thres = float(thres)
+
+   #thres = os.path.normpath('data' + os.sep +'thresh.txt')
+   try:
+      thres = float(thres)
+      print("Single threshold applied to all classes")
+   except:
+      try:
+         with open(thres) as f: #'labels.txt') as f:
+            reader = csv.reader(f)
+            thres = {int(rows[0]):float(rows[1]) for rows in reader}
+            print("Found dictonary of thresholds")
+      except:
+         thres=float(0.9)
+         print('Could not parse threshold csv, reverting to 0.9 threshold')
    thin = float(thin)
 
    #===============================================
    # Run main application
-   Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing   
-   files = askopenfilename(filetypes=[("pick mat files","*.mat")], multiple=True)  
-    
+   Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+   files = askopenfilename(filetypes=[("pick mat files","*.mat")], multiple=True)
+
    direc = imdirec = os.path.dirname(files[0])##'useimages'
-   
+
    #=======================================================
-   outpath = direc+os.sep+'tile_'+str(tile)
+   outpath = os.path.normpath(direc+os.sep+'tile_'+str(tile))
    ##files = sorted(glob(direc+os.sep+'*.mat'))
 
    print("Searching for labels in all %i files" % (len(files)))
@@ -101,8 +127,8 @@ if __name__ == '__main__':
          labels = dat['labels']
          labels = [label.replace(' ','') for label in labels]
          L.extend(labels)
-	
-   labels = np.unique(L).tolist()	
+
+   labels = np.unique(L).tolist()
    #=======================================================
 
    #=======================================================
@@ -113,55 +139,53 @@ if __name__ == '__main__':
 
    for f in labels:
       try:
-         os.mkdir(outpath+os.sep+f)
+         os.mkdir(os.path.normpath(outpath+os.sep+f))
       except:
          pass
    #=======================================================
 
-   types = (direc+os.sep+'*.jpg', direc+os.sep+'*.jpeg', direc+os.sep+'*.tif', direc+os.sep+'*.tiff', direc+os.sep+'*.png') # the tuple of file types
+   types = (os.path.normpath(direc+os.sep+'*.jpg'), os.path.normpath(direc+os.sep+'*.jpeg'), \
+            os.path.normpath(direc+os.sep+'*.tif'), os.path.normpath(direc+os.sep+'*.tiff'), \
+            os.path.normpath(direc+os.sep+'*.png')) # the tuple of file types
    files_grabbed = []
    for f in types:
-      files_grabbed.extend(glob(f))	   
-   
+      files_grabbed.extend(glob(f))
+
    #=======================================================
    for f in files:
       print('Working on %s' % f)
       dat = loadmat(f)
       if 'labels' in dat.keys():
          labels = dat['labels']
-         labels = [label.replace(' ','') for label in labels]	  	  
-	  
+         labels = [label.replace(' ','') for label in labels]
+
       res = dat['class']
       del dat
-      core = f.split('/')[-1].split('_mres')[0]  
+      core = f.split('/')[-1].split('_mres')[0]
 
-      ##get the file that matches the above pattern but doesn't contain 'mres'	  
+      ##get the file that matches the above pattern but doesn't contain 'mres'
       fim = [e for e in files_grabbed if e.find(core)!=-1 if e.find('mres')==-1 ]
       if fim:
          fim = fim[0]
          print('Generating tiles from dense class map ....')
-         Z,ind = sliding_window(imread(fim), (tile,tile,3), (int(tile/2), int(tile/2),3)) 
+         Z,ind = sliding_window(imread(fim), (tile,tile,3), (int(tile/2), int(tile/2),3))
 
-         C,ind = sliding_window(res, (tile,tile), (int(tile/2), int(tile/2))) 
+         C,ind = sliding_window(res, (tile,tile), (int(tile/2), int(tile/2)))
 
-         w = Parallel(n_jobs=-1, verbose=0, pre_dispatch='2 * n_jobs', max_nbytes=None)(delayed(writeout)(Z[k], C[k], labels, outpath, thres) for k in range(len(Z))) 
+         w = Parallel(n_jobs=-1, verbose=0, pre_dispatch='2 * n_jobs', max_nbytes=None)(delayed(writeout)(Z[k], C[k], labels, outpath, thres) for k in range(len(Z)))
       else:
-         print("correspodning image not found")	     
-		 
+         print("correspodning image not found")
+
    print('thinning files ...')
    if thin>0:
-      for f in labels:   
+      for f in labels:
          files = glob(outpath+os.sep+f+os.sep+'*.jpg')
-         if len(files)>60:   
-            usefiles = np.random.choice(files, int(thin*len(files)), replace=False)   
-            rmfiles = [x for x in files if x not in usefiles.tolist()] 
+         if len(files)>60:
+            usefiles = np.random.choice(files, int(thin*len(files)), replace=False)
+            rmfiles = [x for x in files if x not in usefiles.tolist()]
             for rf in rmfiles:
                os.remove(rf)
-	  
+
    for f in labels:
       files = glob(outpath+os.sep+f+os.sep+'*.jpg')
       print(f+': '+str(len(files)))
-  
-
-   
-
